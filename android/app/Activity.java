c563d4719b2172e1fb441386584d4ba6f5568419
megasonic5 <168662773+megasonic5@users.noreply.github.com>
2025-05-22 08:23:36 +0200

    Create Activity.java

---
+++ android/app/Activity.java
@@ -0,0 +1,14 @@
+package android.app;
+
+import android.os.Bundle;
+
+public class Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {}
+    
+    @Override
+    protected void onStart() {}
+
+    @Override
+    protected void onPause() {}
+}
